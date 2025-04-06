c240f7a33cbcbbe85241a2d93a053252157041ce
mohamedzebib22 <mohamed_zebib@yahoo.com>
2025-04-06 16:18:44 +0200

    fix issue for add user

--- lib/Features/home_page/data/cubits/get_and_delete_user_cubit/get_and_delete_user_cubit.dart
+++ lib/Features/home_page/data/cubits/get_and_delete_user_cubit/get_and_delete_user_cubit.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elmanfy/Features/home_page/data/Repos/get_user/get_user_repo.dart';
 import 'package:elmanfy/Features/home_page/data/cubits/get_and_delete_user_cubit/get_and_delete_user_state.dart';
 import 'package:elmanfy/Features/home_page/presentation/views/screens/home_page.dart';
-import 'package:elmanfy/Features/login_page/presentation/views/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
@@ -36,6 +35,7 @@ class GetUserCubit extends Cubit<GetAndDeleteUserState> {
     }, (response){
       print('==========User Delete Sucsess===========');
       emit(GetUserSucsess());
+     
       Navigator.pushNamedAndRemoveUntil(context, HomePage.id, (route) => false);
     });
   }
